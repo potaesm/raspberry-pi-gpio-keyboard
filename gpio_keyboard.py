1177a0625efe99f14f2ab3a3125ff530058076d4
Suthinan Musitmani <suthinan@Suthinans-MacBook-Pro-2.local>
2021-05-06 23:02:57 +0700

    Refactor

--- gpio_keyboard.py
+++ gpio_keyboard.py
@@ -1,63 +1,59 @@
-# Dependency
-# pip3 install pynput
-
-# Usage
-# DISPLAY=:0.0 python3 gpio_keyboard.py
-
 import RPi.GPIO as GPIO
-import time
 import uinput as ui
+from time import sleep
 from os import system
 
+
+class KeyGpio:
+    def __init__(self, device, key, gpio):
+        self.device = device
+        self.key = key
+        # Configure the gpio to pull up
+        GPIO.setup(gpio, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        # Map events to callbacks
+        GPIO.add_event_detect(
+            gpio, GPIO.FALLING, callback=self.callback, bouncetime=100)
+
+    def callback(self, channel):
+        # Debounce and check if gpio pull to GND
+        sleep(0.01)
+        if GPIO.input(channel) == 0:
+            self.device.emit_click(self.key)
+
+
+# Load uinput module in kernel
 system("modprobe uinput")
 
+# Use Broadcom numbers
 GPIO.setmode(GPIO.BCM)
 
 # Assign variable for right and left buttons
-green_button = 16
-red_button = 20
-black_button = 21
+GREEN_BUTTON_GPIO = 16
+RED_BUTTON_GPIO = 20
+BLACK_BUTTON_GPIO = 21
 
-bindings = ((ui.KEY_R, red_button),(ui.KEY_B, black_button),(ui.KEY_ENTER, green_button))
-# create uinput device
-events = list()
-for(key,gpio) in bindings:
-	events.append(key)
-device = ui.Device(events)
+# Bind key with gpio
+bindings = (
+    (ui.KEY_ENTER, GREEN_BUTTON_GPIO),
+    (ui.KEY_R, RED_BUTTON_GPIO),
+    (ui.KEY_B, BLACK_BUTTON_GPIO)
+)
+# Create uinput device
+device = ui.Device([key for (key, gpio) in bindings])
 
-#configure the button pins to pull up 
-GPIO.setup(green_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(red_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(black_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Map key to gpio
+for (key, gpio) in bindings:
+    KeyGpio(device, key, gpio)
 
-# define callbacks
-def onGreenButton(channel):
-    print('Green button pressed')
-    device.emit_click(ui.KEY_ENTER)
-
-def onRedButton(channel):
-    print('Red Button Pressed')
-    device.emit_click(ui.KEY_R)
-
-def onBlackButton(channel):
-    print('Black button pressed')
-    device.emit_click(ui.KEY_B)
-
-
-# Assign callback to button press event
-GPIO.add_event_detect(green_button, GPIO.FALLING, callback=onGreenButton, bouncetime=300)
-GPIO.add_event_detect(red_button, GPIO.FALLING, callback=onRedButton, bouncetime=300)
-GPIO.add_event_detect(black_button, GPIO.FALLING, callback=onBlackButton, bouncetime=300)
-
-
-# this while loop will make sure the script runs forever
+# Run this script forever
 try:
     while True:
-        time.sleep(0.2)
-
+        sleep(0.2)
 except KeyboardInterrupt:
-    # clean up GPIO on CTRL+C exit
+    # Clean up GPIO on CTRL+C exit
+    device.destroy()
     GPIO.cleanup()
 
-# clean up GPIO on normal exit
+# Clean up GPIO on normal exit
+device.destroy()
 GPIO.cleanup()
